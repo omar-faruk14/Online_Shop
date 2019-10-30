3626b47844074b2142518b1b716d76949685cb41
Omarfaruk24 <omarfaruk885@gmail.com>
2019-10-30 12:42:56 +0600

    all_categories.blade.php required html file include

---
+++ resources/views/Admin/all_categories.blade.php
@@ -0,0 +1,57 @@
+@extends('admin.admin_layout')
+@section('admin_content')
+
+
+
+<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a> 
+					<i class="icon-angle-right"></i>
+				</li>
+				<li><a href="#">Tables</a></li>
+			</ul>
+
+			<div class="row-fluid sortable">		
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
+						<div class="box-icon">
+							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
+							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
+						</div>
+					</div>
+					<div class="box-content">
+						<table class="table table-striped table-bordered bootstrap-datatable datatable">
+						  <thead>
+							  <tr>
+								  <th>Categories ID</th>
+								  <th>Categories Name</th>
+								  <th>Categories Description</th>
+								  <th>Status</th>
+								  <th>Actions</th>
+							  </tr>
+						  </thead> 
+
+
+
+
+
+
+						 
+
+
+
+
+					  </table>            
+					</div>
+				</div><!--/span-->
+			
+			</div><!--/row-->
+
+			
+
+
+
+@endsection
