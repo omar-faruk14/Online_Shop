d13af0e219f2825a37c9ede4c0c9d14a1b8d5123
btnirob <btnirob13@gmail.com>
2019-11-04 12:21:02 +0600

    Fronend initial files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Online_Shop</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  .fakeimg {
+    height: 200px;
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <h1>Onile Shop is a place of your desire</h1>
+  <p>All kinds of product is available here. Hopfully it will be very userful for you.</p> 
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">OnlineShop</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Latest</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Offers</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">My Account</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="#">Wishlist</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cart</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>    
+    </ul>
+  </div>  
+</nav>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Shop</h2>
+      <h5>Photo of OnlineShop</h5>
+      <div class="fakeimg">Shop Image</div>
+      <p>Thank you for visiting us. ..</p>
+      <h3>Category</h3>
+      
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Electronics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Robotics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tools</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#"></a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Find your Product here!</h2>
+      <h5>Search  your product, </h5>
+      <div class="fakeimg"> Image</div>
+      <p>Thanks..</p>
+      <p>Your visit is very precious to us. Hope you will be satisfied.Your visit is very precious to us. Hope you will be satisfied.Your visit is very precious to us. Hope you will be satisfied</p>
+      <br>
+      <h2>Your Part</h2>
+      <h5>You can add your own shop product</h5>
+      <div class="fakeimg">Image</div>
+      <p>Nothing..</p>
+      <p>Your visit is very precious to us. Hope you will be satisfied.Your visit is very precious to us. Hope you will be satisfied.Your visit is very precious to us. Hope you will be satisfied.Your visit is very precious to us. Hope you will be satisfied</p>
+    </div>
+  </div>
+</div>
+
+<div class="jumbotron text-center" style="margin-bottom:0">
+  <p>Footer</p>
+</div>
+
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Online Shop</title>
+    <style>
+    
+    
+    </style>
+
+</head>
+<body>
+  <nav class="">
+
+  </nav>
+<ul>
+    <li><a href="/Laravel/Laravel/public">Home</li></a>
+    <li><a href="profile">Profile</li></a>
+    <li><a href="about">About</li></a>
+    <li><a href="test">Test</li></a>
+    
+</ul>    
+ 
+
+</body>
+
+
+
+
+</html>
