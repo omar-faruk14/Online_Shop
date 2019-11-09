d143b0b7a3338f5922e14e908f31ff1b60301eac
Omarfaruk24 <omarfaruk885@gmail.com>
2019-11-09 19:58:08 +0600

    Requred Html include all manufacture page

--- resources/views/Admin/all_manufacture.blade.php
+++ resources/views/Admin/all_manufacture.blade.php
@@ -1,1 +1,73 @@
-all_manufacture.blade.php
+@extends('admin.admin_layout')
+@section('admin_content')
+
+
+
+<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a> 
+					<i class="icon-angle-right"></i>
+				</li>
+				<li><a href="#">Tables</a></li>
+			</ul>
+
+			<div class="row-fluid sortable">		
+				<div class="box span12">
+					<div class="box-header" data-original-title>
+						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
+						<div class="box-icon">
+							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
+							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
+							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
+						</div>
+					</div>
+
+					<p class="alert-success">
+
+                   <!-- session message showing Here -->
+					<?php 
+					$message=Session::get('message');
+
+					if($message)
+					{
+						echo $message;
+						Session::put('message',null);
+
+
+					}
+					 ?>
+					</p>
+
+
+
+
+
+					<div class="box-content">
+						<table class="table table-striped table-bordered bootstrap-datatable datatable">
+						  <thead>
+							  <tr>
+								  <th>Manufacture ID</th>
+								  <th>Manufacture Name</th>
+								  <th>Manufacture Description</th>
+								  <th>Status</th>
+								  <th>Actions</th>
+							  </tr>
+						  </thead> 
+
+						 
+
+
+
+
+					  </table>            
+					</div>
+				</div><!--/span-->
+			
+			</div><!--/row-->
+
+			
+
+
+
+@endsection
