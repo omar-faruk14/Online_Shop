8ffcc8a6b3186a62987e71f5deb7991a8f929f0c
Omarfaruk24 <omarfaruk885@gmail.com>
2019-11-09 21:58:23 +0600

    ProductController,add_product,all product page created

--- resources/views/Admin/admin_layout.blade.php
+++ resources/views/Admin/admin_layout.blade.php
@@ -346,8 +346,8 @@
 						<li>
 							<a class="dropmenu" href="#"><i class="icon-folder-close-alt"></i><span class="hidden-tablet"> Product</span><span class="label label-important"> New </span></a>
 							<ul>
-								<li><a class="submenu" href="submenu.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> All Product</span></a></li>
-								<li><a class="submenu" href="submenu2.html"><i class="icon-file-alt"></i><span class="hidden-tablet"> Add Product</span></a></li>
+								<li><a class="submenu" href="{{URL::to('/all_product')}}"><i class="icon-file-alt"></i><span class="hidden-tablet"> All Product</span></a></li>
+								<li><a class="submenu" href="{{URL::to('/add_product')}}"><i class="icon-file-alt"></i><span class="hidden-tablet"> Add Product</span></a></li>
 								
 							</ul>	
 						</li>
---
+++ resources/views/Admin/all_product.blade.php
@@ -0,0 +1,1 @@
+all_product.blade.php
