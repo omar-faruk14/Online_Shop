7fc45ed8ed191c5e2d751e6f69db258dcaa16456
Omarfaruk24 <omarfaruk885@gmail.com>
2019-11-09 19:07:33 +0600

    manufacture Database Create

--- app/Http/Controllers/ManufactureController.php
+++ app/Http/Controllers/ManufactureController.php
@@ -4,16 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class ManufactureController extends Controller
 {
+	//go to add manufacture page
     public function index()
     {
-    	return view('admin.add_manufacture');
+    	return view('admin.add_manufacture'); 
     }
 
 
-    public function save_manufacture()
+    public function save_manufacture(Request $request)
     {
-    	echo "test";
+    	$data=array();
+
+         $data['Categories_id']=$request->Categories_id;
+         $data['Categories_name']=$request->Categories_name;
+         $data['Categories_description']=$request->Categories_description;
+         $data['Publication_status']=$request->Publication_status;
+
+         DB::table('tbl_category')->insert($data);
+         Session::put('message','Add Category Successfully !');
+         return Redirect('/add_categories');
     }
 }
