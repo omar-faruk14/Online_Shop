d5069212087e7a741dbe3db04e39d7fb97b05b67
Omarfaruk24 <omarfaruk885@gmail.com>
2019-11-10 12:56:41 +0600

    Add Product include form

--- resources/views/Admin/add_product.blade.php
+++ resources/views/Admin/add_product.blade.php
@@ -2,80 +2,133 @@
 @section('admin_content')
 
 <ul class="breadcrumb">
-				<li>
-					<i class="icon-home"></i>
-					<a href="index.html">Home</a>
-					<i class="icon-angle-right"></i> 
-				</li>
-				<li>
-					<i class="icon-edit"></i>
-					<a href="#">Add Categories</a>
-				</li>
-			</ul>
+	<li>
+		<i class="icon-home"></i>
+		<a href="index.html">Home</a>
+		<i class="icon-angle-right"></i> 
+	</li>
+	<li>
+		<i class="icon-edit"></i>
+		<a href="#">Add Categories</a>
+	</li>
+</ul>
+
+<div class="row-fluid sortable">
+	<div class="box span12">
+		<div class="box-header" data-original-title>
+			<h2><i class="halflings-icon edit"></i><span class="break"></span>Add Product</h2>
 			
-			<div class="row-fluid sortable">
-				<div class="box span12">
-					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon edit"></i><span class="break"></span>Add Categories</h2>
-						
-					</div>
+		</div>
 
 
-                     <p class="alert-success">
+         <p class="alert-success">
 
 
-					<?php 
-					$message=Session::get('message');
+		<?php 
+		$message=Session::get('message');
 
-					if($message)
-					{
-						echo $message;
-						Session::put('message',null);
-
-
-					}
-					 ?>
-					</p>
-
-					<div class="box-content">
-						<form class="form-horizontal" action="{{url('/save_categories')}}" method="post">
-							{{ csrf_field()}}
-						  <fieldset>
-							
-							<div class="control-group">
-							  <label class="control-label" for="date01">Categories Name</label>
-							  <div class="controls">
-								<input type="text" class="input-xlarge" name="Categories_name" required="">
-							  </div>
-							</div>
-         
-							<div class="control-group hidden-phone">
-							  <label class="control-label" for="textarea2">Categories Description</label>
-							  <div class="controls">
-								<textarea class="cleditor" name="Categories_description" rows="3" required=""></textarea>
-							  </div>
-							</div>
-
-							<div class="control-group hidden-phone">
-							  <label class="control-label" for="textarea2">Publication Status</label>
-							  <div class="controls">
-								<input type="checkbox" name="Publication_status" value="1" required="">
-							  </div>
-							</div>
+		if($message)
+		{
+			echo $message;
+			Session::put('message',null);
 
 
+		}
+		 ?>
+		</p>
 
-							<div class="form-actions">
-							  <button type="submit" class="btn btn-primary">Add Categories</button>
-							  <button type="reset" class="btn">Cancel</button>
-							</div>
-						  </fieldset>
-						</form>   
-
-					</div>
+		<div class="box-content">
+			<form class="form-horizontal" action="" method="post">
+				{{ csrf_field()}}
+			  <fieldset>
+				
+				
+				<div class="control-group">
+				  <label class="control-label" for="date01">product Name</label>
+				  <div class="controls">
+					<input type="text" class="input-xlarge" name="product_name" required="">
+				  </div>
 				</div>
 
-			</div>
+
+
+
+
+
+			<div class="control-group">
+				<label class="control-label" for="selectError3">Product Category</label>
+				<div class="controls">
+				  <select id="selectError3">
+					<option>Option 1</option>
+					<option>Option 2</option>
+					<option>Option 3</option>
+					<option>Option 4</option>
+					<option>Option 5</option>
+				  </select>
+				</div>
+			  </div>
+
+
+
+
+
+
+			  <div class="control-group">
+				<label class="control-label" for="selectError3">Manufacture Name</label>
+				<div class="controls">
+				  <select id="selectError3">
+					<option>Option 1</option>
+					<option>Option 2</option>
+					<option>Option 3</option>
+					<option>Option 4</option>
+					<option>Option 5</option>
+				  </select>
+				</div>
+			  </div>
+
+
+
+
+				<div class="control-group hidden-phone">
+				  <label class="control-label" for="textarea2">Product Short description</label>
+				  <div class="controls">
+					<textarea class="cleditor" name="product_description" rows="3" required=""></textarea>
+				  </div>
+				</div>
+
+
+
+
+				<div class="control-group hidden-phone">
+				  <label class="control-label" for="textarea2">Product Long description</label>
+				  <div class="controls">
+					<textarea class="cleditor" name="product_description" rows="3" required=""></textarea>
+				  </div>
+				</div>
+
+
+
+
+				<div class="control-group hidden-phone">
+				  <label class="control-label" for="textarea2">product Status</label>
+				  <div class="controls">
+					<input type="checkbox" name="product_status" value="1" required="">
+				  </div>
+				</div>
+
+
+
+				<div class="form-actions">
+				  <button type="submit" class="btn btn-primary">Add product</button>
+				  <button type="reset" class="btn">Cancel</button>
+				</div>
+			  </fieldset>
+			</form>   
+
+		</div>
+	</div>
+
+</div>
 
 
 @endsection
