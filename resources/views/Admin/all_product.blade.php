9642379f784216239cdccd6152e6c911a1317361
Omarfaruk24 <omarfaruk885@gmail.com>
2019-11-11 20:04:36 +0600

    All product page insert Html

--- resources/views/Admin/all_product.blade.php
+++ resources/views/Admin/all_product.blade.php
@@ -1,1 +1,124 @@
-all_product.blade.php
+@extends('admin.admin_layout')
+@section('admin_content')
+
+
+
+<ul class="breadcrumb">
+	<li>
+	<i class="icon-home"></i>
+	<a href="index.html">Home</a> 
+	<i class="icon-angle-right"></i>
+	</li>
+	<li><a href="#">Tables</a></li>
+	</ul>
+
+	<div class="row-fluid sortable">		
+	<div class="box span12">
+	<div class="box-header" data-original-title>
+		<h2><i class="halflings-icon user"></i><span class="break"></span>All Categories List</h2>
+		<div class="box-icon">
+			<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
+			<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
+			<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
+		</div>
+	</div>
+
+	<p class="alert-success">
+
+
+	<?php 
+	$message=Session::get('message');
+
+	if($message)
+	{
+		echo $message;
+		Session::put('message',null);
+
+
+	}
+	 ?>
+	</p>
+
+
+
+
+
+	<div class="box-content">
+		<table class="table table-striped table-bordered bootstrap-datatable datatable">
+		  <thead>
+			  <tr>
+				  <th>Product ID</th>
+				  <th>Product Name</th>
+				  <th>Product Description</th>
+				  <th>Product Image</th>
+				  <th>Product Price</th>
+				   <th>Category Name</th>
+				  <th>Manufacture Name</th>
+				  <th>Status</th>
+				  <th>Actions</th>
+			  </tr>
+		  </thead> 
+
+		  @foreach($all_category_info as $v_category)
+
+
+
+		  <tbody>
+			<tr>
+				<td>{{$v_category->Categories_id}}</td>
+				<td class="center">{{$v_category->Categories_name}}</td>
+				<td class="center">{{$v_category->Categories_description}}</td>
+				<td class="center">
+					@if($v_category->Publication_status==1)
+					<span class="label label-success">Active</span>
+
+					@else  
+					<span class="label label-danger">
+					Unactive</span>
+
+					@endif
+				</td>
+				<td class="center">
+					
+
+					@if($v_category->Publication_status==1)
+					<a class="btn btn-danger" href="{{URL::to('/unactive_category/'.$v_category->Categories_id)}}">
+						<i class="halflings-icon white thumbs-down"></i>  
+					</a>
+
+
+					@else
+	                   <a class="btn btn-success" href="{{URL::to('/active_category/'.$v_category->Categories_id)}}">
+						<i class="halflings-icon white thumbs-up"></i>  
+					    </a>
+
+
+					@endif
+
+
+					<a class="btn btn-info" href="{{URL::to('/edit_category/'.$v_category->Categories_id)}}">
+						<i class="halflings-icon white edit"></i>  
+					</a>
+					<a class="btn btn-danger" href="{{URL::to('/delete_category/'.$v_category->Categories_id)}}">
+						<i class="halflings-icon white trash"></i> 
+					</a>
+				</td>
+			</tr>
+			
+		  </tbody>
+		  @endforeach
+
+
+
+
+	  </table>            
+	</div>
+	</div><!--/span-->
+
+	</div><!--/row-->
+
+			
+
+
+
+@endsection
