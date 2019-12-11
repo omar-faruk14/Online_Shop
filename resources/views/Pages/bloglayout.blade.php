3f4b6946b0838348a80acf07e51843cbbf52f9da
btnirob <bayazid.talukder@northsouth.edu>
2019-12-11 14:57:19 +0600

    Some files updated

--- resources/views/Pages/bloglayout.blade.php
+++ resources/views/Pages/bloglayout.blade.php
@@ -166,11 +166,8 @@
                             <ul class="nav navbar-nav collapse navbar-collapse">
                                 <li><a href="{{URL::to('/')}}" class="active">Home</a></li>
                                 
-                                <li class="dropdown"><a href="/blog">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
+                                <li class="dropdown"><a href="/blog">Blog</a>
+                                   
                                 </li> 
                                 <li><a href="404.html">404</a></li>
                                 <li><a href="contact-us.html">Contact</a></li>
--- resources/views/Pages/layout.blade.php
+++ resources/views/Pages/layout.blade.php
@@ -86,7 +86,7 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="logo pull-left">
-                            <a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+                            <a href="/"><img src="images/home/logo.png" alt="" /></a>
                         </div>
                         
                     </div>
@@ -166,11 +166,8 @@
                             <ul class="nav navbar-nav collapse navbar-collapse">
                                 <li><a href="{{URL::to('/')}}" class="active">Home</a></li>
                                 
-                                <li class="dropdown"><a href="/blog">Blog<i class="fa fa-angle-down"></i></a>
-                                    <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
-                                    </ul>
+                                <li class="dropdown"><a href="/blog">Blog</a>
+                                    
                                 </li> 
                                 <li><a href="404.html">404</a></li>
                                 <li><a href="contact-us.html">Contact</a></li>
