36fdbbd04a2ad3bdf296461347bded9dd38d73df
btnirob <bayazid.talukder@northsouth.edu>
2019-12-11 15:16:22 +0600

    Top menu changed including new links provided

--- resources/views/Pages/bloglayout.blade.php
+++ resources/views/Pages/bloglayout.blade.php
@@ -65,8 +65,8 @@
                         <div class="social-icons pull-right">
                             <ul class="nav navbar-nav">
                                 <li><a href="www.facebook.com"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="www.twitter"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
                                 <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
                                 <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
                             </ul>
