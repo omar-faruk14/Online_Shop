4f40ebfd774de2ee112d225fb75475753a8bdf3a
azheruddin1712351042 <tonmoyazher@gmail.com>
2019-12-18 19:53:05 +0600

    Database record testing-match record

---
+++ tests/Unit/DatabaseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertDatabaseHas('tbl_products',[
+            'product_name'=> "Iphone 6"
+        ]);
+    }
+}
