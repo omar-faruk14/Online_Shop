520de656763ea39ed1d94aff973f7b7838d8d34e
btnirob <bayazid.talukder@northsouth.edu>
2019-12-20 19:36:37 +0600

    Searching and fixing

--- resources/views/Pages/blog.blade.php
+++ resources/views/Pages/blog.blade.php
@@ -18,13 +18,13 @@
 
 foreach($all_published_product as $v_product) {?>
        
-       <div class="card" style="margin:5px;padding:5px;border:2px solid #f1f1f1;">
+       <div class="card" style="margin:5px;padding:5px;border:2px solid #f1f1f1;min-height:200px;">
             <div class="card-body">
                        
                             <img src="{{URL::to($v_product->product_image)}}" style="height: 207px; width: 183px" alt="" />
                             
                         <div class="col-lg-9" style="float:right; margin-bottom:5px; padding:5px;">
-                            <p>{{($v_product->product_name)}}</p>
+                            <p style="font-size:20px;color:blue">{{($v_product->product_name)}}</p>
                                 {{($v_product->product_long_description)}}
                         </div>
                         
